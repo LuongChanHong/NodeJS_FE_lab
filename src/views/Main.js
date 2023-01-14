e05a5aa3d42491d3b5de18546f1f0d7b9615dbf0
honglc <hongmainmail@gmail.com>
2023-01-14 11:08:05 +0700

    lab 21.1 delete unnecessary parts

--- src/views/Main.js
+++ src/views/Main.js
@@ -5,13 +5,6 @@ import "../css/main.css";
 import "../App.css";
 import { Routes, Route } from "react-router-dom";
 
-import AddProduct from "./AddProduct";
-import Products from "./Products";
-import ProductDetail from "./ProductDetail";
-import AdminProducts from "./AdminProducts";
-import Shop from "./Shop";
-import Cart from "./Cart";
-import Order from "./Order";
 import Login from "./Login";
 import Signup from "./Signup";
 
@@ -19,19 +12,10 @@ const Main = () => {
   return (
     <section className="app_bg">
       <Routes>
-        <Route exact path="/products" element={<Products />} />
-        <Route exact path="/shop" element={<Shop />} />
-        <Route exact path="/cart" element={<Cart />} />
         <Route exact path="/login" element={<Login />} />
         <Route exact path="/signup" element={<Signup />} />
 
-        <Route exact path="/admin-products" element={<AdminProducts />} />
-        <Route exact path="/products/:productID" element={<ProductDetail />} />
-        <Route exact path="/edit/:productID" element={<AddProduct />} />
-
-        <Route exact path="/add-product" element={<AddProduct />} />
-        <Route exact path="/order" element={<Order />} />
-        <Route path="*" element={<Shop />} />
+        <Route path="*" element={<Login />} />
       </Routes>
     </section>
   );
